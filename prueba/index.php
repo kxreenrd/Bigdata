a3d4612273043f6d69684953eb284a9d1d500a8d
Karen Rodríguez <krodrigue51@uniminuto.edu.co>
2019-07-18 17:28:13 -0500

    Add files via upload

---
+++ prueba/index.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet">
+        
+        
+        
+        <!-- Optional JavaScript -->
+        <script src="js/functions.js"></script>
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        
+        <title>Proyecto Big Data</title>
+    </head>
+    <body id="page-top">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">Proyecto Big Data</a>
+<!--                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>-->
+<!--                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#registro">Registro</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="#lista">Lista</a>
+                        </li>
+                    </ul>
+                </div>-->
+            </div>
+        </nav>
+
+        <header class="bg-primary text-white">
+            <div class="container text-center">
+                <h3>Construcción de un modelo predictivo para casos de violencia intrafamiliar en los departamentos de Colombia para el año 2020 en un ambiente big data.</h3>
+               <p class="lead">Aqui iría un breve introducción de la problematica del proyecto y lo que se quiere mostrar</p>
+            </div>
+        </header>
+
+        <section id="registro">
+            <div class="container">
+                <div class="card col-sm-3" style="float:left">
+                    <div class="card-body">
+                        <h4 class="card-title">Año 2016</h4>
+                        <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+<!--                        <a href="#" class="btn btn-primary">See Profile</a>-->
+                    </div>
+                    <img class="card-img-bottom" src="img/grafica1.jpg" alt="Card image" style="width:100%">
+                </div>
+                <div class="card col-sm-3" style="float:left">
+                    <div class="card-body">
+                        <h4 class="card-title">Año 2017</h4>
+                        <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+<!--                        <a href="#" class="btn btn-primary">See Profile</a>-->
+                    </div>
+                    <img class="card-img-bottom" src="img/grafica1.jpg" alt="Card image" style="width:100%">
+                </div>
+                <div class="card col-sm-3" style="float:left">
+                    <div class="card-body">
+                        <h4 class="card-title">Año 2018</h4>
+                        <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+<!--                        <a href="#" class="btn btn-primary">See Profile</a>-->
+                    </div>
+                    <img class="card-img-bottom" src="img/grafica1.jpg" alt="Card image" style="width:100%">
+                </div>
+                <div class="card col-sm-3" >
+                    <div class="card-body">
+                        <h4 class="card-title">Año 2019</h4>
+                        <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+<!--                        <a href="#" class="btn btn-primary">See Profile</a>-->
+                    </div>
+                    <img class="card-img-bottom" src="img/grafica1.jpg" alt="Card image" style="width:100%">
+                </div>
+                
+            </div>
+            
+        </section>
+        
+
+        <!-- Footer -->
+        <footer class="py-5 bg-dark">
+            <div class="container">
+                <p class="m-0 text-center text-white">Copyright &copy; Daniel Contreras & Karen Rodríguez - 2019 - UNIMINUTO</p>
+            </div>
+            <!-- /.container -->
+        </footer>
+        <script>
+            $("#identificacion").keypress(function(){
+                var cedula=($(this).val()).replace(/\D/g, "");
+
+                var cedulas= cedula.replace(/([0-9])([0-9]{3})$/, '$1.$2');
+                        cedulas= cedula.replace(/\B(?=(\d{3})+(?!\d)\.?)/g, ".");
+               $("#identificacion").val(cedulas);
+
+            });
+            
+            /*$("#nombre").keypress(function(){
+		$("input").css("border","1px solid #000");
+		if($("#nombre").val().match(/^[a-zA-Z]+$/)){
+			$(this).css("border","1px solid green");
+			$("#dato").html("");
+		}else{
+			$(this).css("border","1px solid red");
+			$("#dato").html("<p>Por favor solo ingresar letras en el campo nombre<p>");
+		}
+	});*/
+        
+        </script>
+
+
+        
+    </body>
+</html>
